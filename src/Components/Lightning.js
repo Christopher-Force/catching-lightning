0fb6cff764d770c70746d0f9494e90b27de4701e
Christopher-Force <chrisforceproduction@outlook.com>
2022-02-14 02:40:41 -0500

    vid resized to fit mobile screen

--- src/Components/Lightning.js
+++ src/Components/Lightning.js
@@ -4,7 +4,7 @@ export default function Lightning() {
     return(
         <div className="absolute bg-black -z-10">
             <div className="grid justify-items-center">
-                <video className="sepia brightness-50 min-h-[75vh]" style={{objectFit: "cover"}} autoPlay muted playsInline loop poster="../assets/img/lightning.jpg">
+                <video className="sepia brightness-50 min-h-[100vh] lg:min-h-[75vh]" style={{objectFit: "cover"}} autoPlay muted playsInline loop poster="../assets/img/lightning.jpg">
                     <source id="myvideo" src={LightningVideo} type="video/mp4" />
                 </video>
             </div>
